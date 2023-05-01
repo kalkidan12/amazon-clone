f7883c036579a9ccc9910cd60a2ea398404a24c8
kalkidan12 <kalget12@gmail.com>
2023-05-01 10:13:01 +0300

    backend routes added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 // IMPORTS FROM PACKAGES
 const express = require("express");
 const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
 
 // INIT
 const PORT = process.env.PORT || 3000;
 const app = express();
 const DB = "";
-s;
-
 // middleware
 app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
 
 // Connections
 mongoose
